2e13d167cc21ed9206dc436cfc89d00af859f21a
Indiana MAGNIEZ <indiana.magniez@axyus.com>
2018-05-14 17:26:05 +0200

    Initial commit

---
+++ ColorsFinder.py
@@ -0,0 +1,76 @@
+# !/usr/bin/python
+
+import os
+import os.path
+import sys
+import re
+import fnmatch
+
+def matchAnyPattern(fileName, patternList):
+	for pattern in patternList:
+		if fnmatch.fnmatch(fileName, pattern):
+			return True
+	return False
+
+def findColorsMatches(rootDirectoryPath, regexPattern=u"#[0-9A-Fa-f]{6,8}", fileFilterList=[u"*"], fileIgnoreList=[], dirIgnoreList=[]):
+	colorsMatch = set()
+	for root, dirs, files in os.walk(top=rootDirectoryPath, topdown=True):
+		files = [f for f in files if matchAnyPattern(f,fileFilterList)]
+		files = [f for f in files if not matchAnyPattern(f,fileIgnoreList)]
+		dirs[:] = [d for d in dirs if not matchAnyPattern(d,dirIgnoreList)]
+		for name in files:
+			fileName = os.path.join(root, name)
+			filetext = ""
+			with open(fileName, 'r') as textfile:
+				filetext = textfile.read()
+				textfile.close()
+			for groupFound in re.finditer(regexPattern, filetext):
+				colorsMatch.add(groupFound.group(0).upper())
+	return colorsMatch
+
+def findExtensionSet(rootDirectoryPath, fileFilterList=[u"*"], fileIgnoreList=[], dirIgnoreList=[]):
+	extensionSet = set()
+	for root, dirs, files in os.walk(top=rootDirectoryPath, topdown=True):
+		files = [f for f in files if matchAnyPattern(f,fileFilterList)]
+		files = [f for f in files if not matchAnyPattern(f,fileIgnoreList)]
+		dirs[:] = [d for d in dirs if not matchAnyPattern(d,dirIgnoreList)]
+		for name in files:
+			extensionSet.add(os.path.splitext(name)[1])
+	return extensionSet
+
+if __name__ == '__main__':
+	path = u"."
+	
+	# dirIgnoreList = [u".svn",u".git",u"target",u"sql-archives"]
+	# for root, dirs, files in os.walk(top=path, topdown=True):
+		# for name in dirs:
+			# print(name, u"match pattern in",dirIgnoreList,u"?",matchAnyPattern(name,dirIgnoreList))
+	# sys.exit(0)
+	
+	if len(sys.argv) > 2:
+		sys.exit("Only one argument")
+	elif len(sys.argv) == 2:
+		path = str(sys.argv[1])
+	
+	fileFilterList = list()
+	fileIgnoreList = list()
+	dirIgnoreList = list()
+
+	# fileFilterList = [u"*.java",u"*.less",u"*.css"]
+	# fileIgnoreList = [u"*.log",u"*.pyc",u"*.png"]
+	dirIgnoreList = [u".svn",u".git",u"target",u"sql-archives"]
+
+	if not fileFilterList:
+		fileFilterList = [u"*"]
+	
+	extensionSet = findExtensionSet(path, fileFilterList=fileFilterList, fileIgnoreList=fileIgnoreList, dirIgnoreList=dirIgnoreList)
+	print(u"Extension")
+	for ext in extensionSet:
+		print(u'\t',ext)
+	
+	fileFilterList = [u"*.java",u"*.less",u"*.css",u"*.jspf",u"*.jsp",u"*.html"]
+	
+	colorsMatch = findColorsMatches(path, fileFilterList=fileFilterList, fileIgnoreList=fileIgnoreList, dirIgnoreList=dirIgnoreList)
+	print(u"Color Matches")
+	for color in colorsMatch:
+		print(u'\t',color)
