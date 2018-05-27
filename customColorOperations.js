412478d0f49345cc83a40eda767d1dead811bd58
MessuKilkain <magniez.indiana@gmail.com>
2018-05-27 20:59:35 +0200

    Move operations to separate js file

--- customColorOperations.js
+++ customColorOperations.js
@@ -56,3 +56,106 @@ function buildColorsConvertionMap(colors, sourceW3Color, targetW3Color, findConv
 	}
 	return conversionMap;
 }
+
+function buildColorsProximityGroupList(originalColorDistanceList, colorDistanceMatrix, seuil) {
+	var colorDistanceList = [];
+	for(var key in originalColorDistanceList) {
+		colorDistanceList.push(key);
+	}
+	colorDistanceList = colorDistanceList.sort(function(key1, key2){
+		return originalColorDistanceList[key1] - originalColorDistanceList[key2];
+	});
+	var colorGroups = [];
+	var classedColorSet = new Set();
+	
+	// Build groups as first pass
+	for( var colorDistanceIndex in colorDistanceList ) {
+		var keys = colorDistanceList[colorDistanceIndex];
+		
+		keys = keys.split('-');
+		
+		var color1 = keys[0];
+		var color2 = keys[1];
+		
+		var distance = colorDistanceMatrix[color1][color2];
+		if( 0 < distance && distance <= seuil ) {
+			var found = false;
+			for( var colorGroupIndex in colorGroups ) {
+				var colorGroupSet = colorGroups[colorGroupIndex];
+				if( colorGroupSet.has(color1) || colorGroupSet.has(color2) ) {
+					found = true;
+					
+					var isCloseEnoughFromEveryOtherColorInGroup = true;
+					for( var colorInSet of colorGroupSet ) {
+						var distanceToColorInSet = colorDistanceMatrix[color1][colorInSet];
+						isCloseEnoughFromEveryOtherColorInGroup = isCloseEnoughFromEveryOtherColorInGroup && ( 0 <= distanceToColorInSet && distanceToColorInSet <= seuil );
+						if(!isCloseEnoughFromEveryOtherColorInGroup){break;}
+					}
+					for( var colorInSet of colorGroupSet ) {
+						var distanceToColorInSet = colorDistanceMatrix[color2][colorInSet];
+						isCloseEnoughFromEveryOtherColorInGroup = isCloseEnoughFromEveryOtherColorInGroup && ( 0 <= distanceToColorInSet && distanceToColorInSet <= seuil );
+						if(!isCloseEnoughFromEveryOtherColorInGroup){break;}
+					}
+					if(isCloseEnoughFromEveryOtherColorInGroup) {
+						if(!classedColorSet.has(color1)) {
+							colorGroupSet.add(color1);
+							classedColorSet.add(color1);
+						}
+						if(!classedColorSet.has(color2)) {
+							colorGroupSet.add(color2);
+							classedColorSet.add(color2);
+						}
+					}
+					break;
+				}
+			}
+			if(!found) {
+				classedColorSet.add(color1);
+				classedColorSet.add(color2);
+				colorGroups.push(new Set([color1,color2]));
+			}
+		}
+	}
+	
+	// Merge groups
+	var noMergeDone = true;
+	var safeCounter = 0;
+	var safeCounterLimit = colorGroups.length;
+	do{
+		var mergedGroups = [];
+		var mergedGroupIndexes = new Set();
+		for( var colorGroupToMergeIndex = 0 ; colorGroupToMergeIndex < colorGroups.length ; colorGroupToMergeIndex++ ) {
+			if(!mergedGroupIndexes.has(colorGroupToMergeIndex)) {
+				mergedGroupIndexes.add(colorGroupToMergeIndex);
+				var colorGroupSetToMerge = colorGroups[colorGroupToMergeIndex];
+				var newMergedSet = new Set(colorGroupSetToMerge);
+				for( var colorGroupIndex = colorGroupToMergeIndex + 1 ; colorGroupIndex < colorGroups.length ; colorGroupIndex++ ) {
+					if(!mergedGroupIndexes.has(colorGroupIndex)) {
+						var colorGroupSet = colorGroups[colorGroupIndex];
+						var canMerge = true;
+						for( var colorInSetToMerge of newMergedSet ) {
+							for( var colorInSet of colorGroupSet ) {
+								var distanceToColorInSet = colorDistanceMatrix[colorInSetToMerge][colorInSet];
+								canMerge = canMerge && ( distanceToColorInSet <= seuil );
+								if(!canMerge){break;}
+							}
+						}
+						if(canMerge) {
+							mergedGroupIndexes.add(colorGroupIndex);
+							for (var newColor of colorGroupSet) {
+								newMergedSet.add(newColor);
+							}
+						}
+					}
+				}
+				mergedGroups.push(newMergedSet);
+			}
+		}
+		noMergeDone = (colorGroups.length == mergedGroups.length);
+		colorGroups = mergedGroups;
+		
+		safeCounter++;
+	} while(!noMergeDone && safeCounter < safeCounterLimit);
+	
+	return colorGroups;
+}
