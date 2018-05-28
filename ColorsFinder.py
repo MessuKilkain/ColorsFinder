3a28b1e5094daf1cc248eb45956a60a538632735
Indiana MAGNIEZ <indiana.magniez@axyus.com>
2018-05-28 19:20:38 +0200

    Improve restriction

--- ColorsFinder.py
+++ ColorsFinder.py
@@ -12,7 +12,7 @@ def matchAnyPattern(fileName, patternList):
 			return True
 	return False
 
-def findColorsMatches(rootDirectoryPath, regexPattern=u"([^&0-9a-zA-Z]|^)(?P<color>#([0-9A-Fa-f]{8}|[0-9A-Fa-f]{6}|[0-9A-Fa-f]{3}))([^0-9A-Fa-f]|$)", fileFilterList=[u"*"], fileIgnoreList=[], dirIgnoreList=[], colorsMatch=dict()):
+def findColorsMatches(rootDirectoryPath, regexPattern=u"([^&0-9a-zA-Z]|^)(?P<color>#([0-9A-Fa-f]{8}|[0-9A-Fa-f]{6}|[0-9A-Fa-f]{3}))([^0-9A-Za-z]|$)", fileFilterList=[u"*"], fileIgnoreList=[], dirIgnoreList=[], colorsMatch=dict()):
 	for root, dirs, files in os.walk(top=rootDirectoryPath, topdown=True):
 		files = [f for f in files if matchAnyPattern(f,fileFilterList)]
 		files = [f for f in files if not matchAnyPattern(f,fileIgnoreList)]
